e8ae88fcccc1f449a40090ad61fa29e19d7653e8
hugopassos <hugo.passos@hotmail.com>
2019-11-05 18:20:43 -0300

    Fixing stickler errors

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,8 +6,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:                  '',
-                                         email:                 'user@invalid',
+      post users_path, params: { user: { name:  '',
+                                         email: 'user@invalid',
                                          password:              'foo',
                                          password_confirmation: 'bar' } }
     end
@@ -19,8 +19,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'valid signup information' do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:                  'Example User',
-                                         email:                 'user@example.com',
+      post users_path, params: { user: { name:  'Example User',
+                                         email: 'user@example.com',
                                          password:              'password',
                                          password_confirmation: 'password' } }
     end
