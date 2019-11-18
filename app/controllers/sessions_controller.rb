ca2ecbb255d688ec91f7faa95e0e27693faf2654
hugopassos <hugo.passos@hotmail.com>
2019-11-18 19:00:39 -0300

    Fixing stickler errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module SessionsHelper
   # Logs in the given user.
   def log_in(user)
@@ -5,10 +7,12 @@ module SessionsHelper
   end
 
   # Returns the current logged-in user (if any).
+  # This code for current_user the Ruby idiom to the following (more readable, and that's in the book):
+  # if session[:user_id]
+  #  @current_user ||= User.find_by(id: session[:user_id])
+  # end
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+    return @current_user ||= User.find_by(id: session[:user_id]) unless !session[:user_id]
   end
 
   # Returns true if the user is logged in, false otherwise.
