c91ff7ff22dc2e16a526226fd485874d9894c877
hugopassos <hugo.passos@hotmail.com>
2019-11-18 19:08:50 -0300

    Fixing stickler errors

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,12 +7,8 @@ module SessionsHelper
   end
 
   # Returns the current logged-in user (if any).
-  # This code for current_user the Ruby idiom to the following (more readable, and that's in the book):
-  # if session[:user_id]
-  #  @current_user ||= User.find_by(id: session[:user_id])
-  # end
   def current_user
-    return @current_user ||= User.find_by(id: session[:user_id]) unless !session[:user_id]
+    return @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   # Returns true if the user is logged in, false otherwise.
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:hugo)
   end
@@ -20,7 +19,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email: @user.email,
                                           password: '123456' } }
-    assert is_logged_in?
+    assert helper_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
@@ -28,7 +27,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
     delete logout_path
-    assert_not is_logged_in?
+    assert_not helper_logged_in?
     assert_redirected_to root_url
     follow_redirect!
     assert_select 'a[href=?]', login_path
