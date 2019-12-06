1b4fbd600321207a72ca005468e53c7452f81b17
hugopassos <hugo.passos@hotmail.com>
2019-12-06 14:27:52 -0300

    Fixing stickler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
   # Confirms a logged-in user.
   def logged_in_user
     return if logged_in?
-    
+
     store_location
     flash[:danger] = 'Please log in.'
     redirect_to login_url
