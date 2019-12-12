f8b36fed3d288d42e0392f59039c30b712f566b6
hugopassos <hugo.passos@hotmail.com>
2019-12-12 19:20:19 -0300

    Fixing stickler errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
+    redirect_to root_url && return unless @user.activated?
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -57,15 +57,14 @@ class User < ApplicationRecord
   end
 
   private
+  # Converts email to all lower-case.
+  def downcase_email
+    email.downcase!
+  end
 
-    # Converts email to all lower-case.
-    def downcase_email
-      email.downcase!
-    end
-
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
