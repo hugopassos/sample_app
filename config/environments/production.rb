e28848391d2a258c13b920a6e28cf7c703018249
hugopassos <hugo.passos@hotmail.com>
2019-12-12 19:11:09 -0300

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,18 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'evening-cliffs-95661.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ User.create!(name: 'Hugo',
              email: 'hugo@mail.com',
              password: '123456',
              password_confirmation: '123456',
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
@@ -19,5 +21,7 @@ User.create!(name: 'Hugo',
   User.create!(name: name,
                email: email,
                password: password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
